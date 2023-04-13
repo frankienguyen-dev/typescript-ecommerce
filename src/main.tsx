b8abaa3353a78cd31b5116f72edb1c9a884ef479
frankienguyen-dev <binguyen221198@gmail.com>
2023-04-13 08:55:14 +0700

    Handle when token expried

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { AppProvider } from './contexts/app.context'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AppProvider } from "./contexts/app.context";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false
-    }
-  }
-})
+      refetchOnWindowFocus: false,
+      retry: 0,
+    },
+  },
+});
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
@@ -26,4 +27,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,22 +1,27 @@
-import { profile } from 'console'
-import { User } from 'src/types/user.type'
+import { profile } from "console";
+import { User } from "src/types/user.type";
+
+export const LocalStorageEventTarget = new EventTarget();
 
 export const setAccessTokenToLocalStorage = (access_token: string) => {
-  localStorage.setItem('access_token', access_token)
-}
+  localStorage.setItem("access_token", access_token);
+};
 
 export const clearLocalStorage = () => {
-  localStorage.removeItem('access_token')
-  localStorage.removeItem('profile')
-}
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("profile");
+  const clearLocalStorageEvent = new Event("clearLocalStorage");
+  LocalStorageEventTarget.dispatchEvent(clearLocalStorageEvent);
+};
 
-export const getAccessTokenFromLocalStorage = () => localStorage.getItem('access_token') || ''
+export const getAccessTokenFromLocalStorage = () =>
+  localStorage.getItem("access_token") || "";
 
 export const getProfileFromLocalStorage = () => {
-  const result = localStorage.getItem('profile')
-  return result ? JSON.parse(result) : null
-}
+  const result = localStorage.getItem("profile");
+  return result ? JSON.parse(result) : null;
+};
 
 export const setProfileToLocalStorage = (profile: User) => {
-  localStorage.setItem('profile', JSON.stringify(profile))
-}
+  localStorage.setItem("profile", JSON.stringify(profile));
+};
