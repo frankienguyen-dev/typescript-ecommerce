dbbcd97c9fdd085eb1c28ad39b78fed9d55534fd
frankienguyen-dev <binguyen221198@gmail.com>
2023-04-17 13:19:19 +0700

    Declare and test API for profile section

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,13 +1,14 @@
-type Role = 'User' | 'Admin'
+type Role = "User" | "Admin";
 
 export interface User {
-  _id: string
-  roles: Role[]
-  email: string
-  name: string
-  date_of_birth: null
-  address: string
-  phone: string
-  createdAt: string
-  updatedAt: string
+  _id: string;
+  roles: Role[];
+  email: string;
+  name?: string;
+  date_of_birth?: string;
+  avatar?: string;
+  address?: string;
+  phone?: string;
+  createdAt: string;
+  updatedAt: string;
 }
