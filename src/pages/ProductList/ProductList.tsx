59ec79a1210e57fc33ff0f88660c8072cff55583
frankienguyen-dev <binguyen221198@gmail.com>
2023-04-25 14:36:59 +0700

    Show profile data on form

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,58 +1,67 @@
-import AsideFilter from './components/AsideFilter'
-import SortProductList from './components/SortProductList'
-import Product from './components/Product/Product'
-import { useQuery } from '@tanstack/react-query'
-import productApi from 'src/apis/product.api'
-import Pagination from 'src/components/Pagination'
-import { productListConfig } from 'src/types/product.type'
-import categoryApi from 'src/apis/category.api'
-import useQueryConfig from 'src/hooks/useQueryConfig'
+import AsideFilter from "./components/AsideFilter";
+import SortProductList from "./components/SortProductList";
+import Product from "./components/Product/Product";
+import { useQuery } from "@tanstack/react-query";
+import productApi from "src/apis/product.api";
+import Pagination from "src/components/Pagination";
+import { productListConfig } from "src/types/product.type";
+import categoryApi from "src/apis/category.api";
+import useQueryConfig from "src/hooks/useQueryConfig";
 
 export type QueryConfig = {
-  [key in keyof productListConfig]: string
-}
+  [key in keyof productListConfig]: string;
+};
 
 export default function ProductList() {
-  const queryConfig = useQueryConfig()
+  const queryConfig = useQueryConfig();
 
   const { data: productsData } = useQuery({
-    queryKey: ['products', queryConfig],
+    queryKey: ["products", queryConfig],
     queryFn: () => {
-      return productApi.getProducts(queryConfig as productListConfig)
+      return productApi.getProducts(queryConfig as productListConfig);
     },
     keepPreviousData: true,
-    staleTime: 3 * 60 * 1000
-  })
+    staleTime: 3 * 60 * 1000,
+  });
 
   const { data: categoriesData } = useQuery({
-    queryKey: ['categories'],
+    queryKey: ["categories"],
     queryFn: () => {
-      return categoryApi.getCategory()
-    }
-  })
+      return categoryApi.getCategory();
+    },
+  });
 
   return (
-    <div className='bg-gray-200 py-6'>
-      <div className='container'>
+    <div className="bg-gray-200 py-6">
+      <div className="container">
         {productsData && (
-          <div className='grid grid-cols-12 gap-6'>
-            <div className='col-span-2'>
-              <AsideFilter queryConfig={queryConfig} categories={categoriesData?.data.data || []} />
+          <div className="grid grid-cols-12 gap-6">
+            <div className="col-span-2">
+              <AsideFilter
+                queryConfig={queryConfig}
+                categories={categoriesData?.data.data || []}
+              />
             </div>
-            <div className='col-span-10 '>
-              <SortProductList queryConfig={queryConfig} pageSize={productsData.data.data.pagination.page_size} />
-              <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+            <div className="col-span-10 ">
+              <SortProductList
+                queryConfig={queryConfig}
+                pageSize={productsData.data.data.pagination.page_size}
+              />
+              <div className="mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                 {productsData.data.data.products.map((product) => (
-                  <div className='col-span-1' key={product._id}>
+                  <div className="col-span-1" key={product._id}>
                     <Product product={product} />
                   </div>
                 ))}
               </div>
-              <Pagination queryConfig={queryConfig} pageSize={productsData.data.data.pagination.page_size} />
+              <Pagination
+                queryConfig={queryConfig}
+                pageSize={productsData.data.data.pagination.page_size}
+              />
             </div>
           </div>
         )}
       </div>
     </div>
-  )
+  );
 }
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,7 +1,77 @@
-import React from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import React, { useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
+import userApi from "src/apis/user.api";
 import Input from "src/components/Input";
+import InputNumber from "src/components/InputNumber";
+import { UserSchema, userSchema } from "src/utils/rules";
+import DateSelect from "../User/components/DateSelect";
+
+type FormData = Pick<
+  UserSchema,
+  "address" | "avatar" | "date_of_birth" | "name" | "phone"
+>;
+
+const profileSchema = userSchema.pick([
+  "name",
+  "avatar",
+  "phone",
+  "date_of_birth",
+  "address",
+]);
 
 export default function Profile() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    control,
+    formState: { errors },
+    watch,
+    setError,
+  } = useForm<FormData>({
+    defaultValues: {
+      name: "",
+      phone: "",
+      address: "",
+      avatar: "",
+      date_of_birth: new Date(1990, 0, 1),
+    },
+    resolver: yupResolver(profileSchema),
+  });
+
+  const updateProfileMutation = useMutation(userApi.updateProfile);
+
+  const { data: profileData } = useQuery({
+    queryKey: ["profiles"],
+    queryFn: userApi.getProfile,
+  });
+
+  const profile = profileData?.data.data;
+
+  useEffect(() => {
+    if (profile) {
+      setValue("name", profile.name);
+      setValue("phone", profile.phone);
+      setValue("address", profile.address);
+      setValue("avatar", profile.avatar);
+      setValue(
+        "date_of_birth",
+        profile.date_of_birth
+          ? new Date(profile.date_of_birth)
+          : new Date(1990, 0, 1)
+      );
+    }
+  }, [profile, setValue]);
+
+  const onSubmit = handleSubmit(async (data) => {
+    console.log("check data: ", data);
+    // await updateProfileMutation.mutateAsync({
+
+    // })
+  });
+
   return (
     <div className="rounded-sm bg-white px-4 pb-10 shadow md:px-7 md:pb-20">
       <div className="sm: border-b border-b-gray-200 py-6 text-center md:text-left">
@@ -13,14 +83,17 @@ export default function Profile() {
         </div>
       </div>
 
-      <div className="md:flex-row mt-8 flex flex-col-reverse md:items-start">
-        <form className="mt-6 flex-grow md:mt-0 md:pr-12">
+      <form
+        className="mt-8 flex flex-col-reverse md:flex-row md:items-start"
+        onSubmit={onSubmit}
+      >
+        <div className="mt-6 flex-grow md:mt-0 md:pr-12">
           <div className=" flex flex-wrap ">
-            <div className="truncate pr-5 sm:pr-0 pt-3 capitalize sm:w-[20%] sm:text-right">
+            <div className="truncate pr-5 pt-3 capitalize sm:w-[20%] sm:pr-0 sm:text-right">
               Email:
             </div>
             <div className="w-[80%] sm:pl-5">
-              <div className="pt-3 text-gray-700">bi*********98@gmail.com</div>
+              <div className="pt-3 text-gray-700">{profile?.email}</div>
             </div>
           </div>
 
@@ -32,6 +105,10 @@ export default function Profile() {
               <Input
                 classNameInput="w-full rounded-sm border-[1px] border-gray-300 
               px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm"
+                placeholder="Tên"
+                name="name"
+                register={register}
+                errorMessage={errors.name?.message}
               />
             </div>
           </div>
@@ -41,9 +118,19 @@ export default function Profile() {
               Số điện thoại:
             </div>
             <div className="mt-3 w-full sm:mt-0 sm:w-[80%] sm:pl-5">
-              <Input
-                classNameInput="w-full rounded-sm border-[1px] border-gray-300 
+              <Controller
+                control={control}
+                name="phone"
+                render={({ field }) => (
+                  <InputNumber
+                    classNameInput="w-full rounded-sm border-[1px] border-gray-300 
               px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm"
+                    placeholder="Số điện thoại"
+                    errorMessage={errors.phone?.message}
+                    {...field}
+                    onChange={field.onChange}
+                  />
+                )}
               />
             </div>
           </div>
@@ -56,41 +143,41 @@ export default function Profile() {
               <Input
                 classNameInput="w-full rounded-sm border-[1px] border-gray-300 
               px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm"
+                placeholder="Địa chỉ"
+                name="address"
+                register={register}
+                errorMessage={errors.address?.message}
               />
             </div>
           </div>
+          <Controller
+            control={control}
+            name="date_of_birth"
+            render={({ field }) => (
+              <DateSelect
+                errorMessage={errors.date_of_birth?.message}
+                onChange={field.onChange}
+                value={field.value}
+              />
+            )}
+          />
 
-          <div className=" mt-2 flex flex-col flex-wrap sm:flex-row">
-            <div className="w-full truncate pt-3 capitalize sm:w-[20%] sm:text-right">
-              Ngày sinh:
-            </div>
-            <div className="mt-3 w-full sm:mt-0 sm:w-[80%] sm:pl-5">
-              <div className="flex justify-between gap-5">
-                <select
-                  className="h-10 w-[32%] rounded-sm border border-black/10 pl-2 hover:cursor-pointer
-                 hover:border-orange"
-                >
-                  <option disabled>Ngày</option>
-                </select>
-                <select
-                  className="h-10 w-[32%] rounded-sm border border-black/10 pl-2 hover:cursor-pointer
-                 hover:border-orange"
-                >
-                  <option disabled>Tháng</option>
-                </select>
-                <select
-                  className="h-10 w-[32%] rounded-sm border border-black/10 pl-2 hover:cursor-pointer
-                 hover:border-orange"
-                >
-                  <option disabled>Năm</option>
-                </select>
-              </div>
+          <div className=" mt-2 flex flex-col flex-wrap md:flex-row">
+            <div className="w-full truncate pt-3 capitalize sm:w-[20%] sm:text-right" />
+            <div className="mt-3 flex w-full md:mt-0 md:w-[80%] md:justify-center md:pl-5">
+              <button
+                className="mt-3 flex h-12 w-full items-center justify-center rounded-sm 
+                bg-orange px-5 capitalize text-white shadow-sm outline-none hover:bg-orange/90 md:w-[150px] "
+                type="submit"
+              >
+                Lưu
+              </button>
             </div>
           </div>
-        </form>
+        </div>
 
         <div className="flex justify-center md:w-72 md:items-center md:border-l  md:border-l-gray-300 md:py-8">
-          <div className="flex flex-col mb-6 md:mb-0 items-center">
+          <div className="mb-6 flex flex-col items-center md:mb-0">
             <div className="my-5 h-24 w-24">
               <img
                 src="https://cf.shopee.vn/file/sg-11134004-23030-h944s18hilovdf_tn"
@@ -102,6 +189,7 @@ export default function Profile() {
             <button
               className="flex h-10 items-center justify-end rounded-sm border 
             bg-white px-6 text-sm text-gray-600 shadow-sm"
+              type="button"
             >
               Chọn Ảnh
             </button>
@@ -112,7 +200,7 @@ export default function Profile() {
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
